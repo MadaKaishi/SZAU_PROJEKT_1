5006f87fd9f47dfef008980bb2091712cdd238d8
Adam <01158187@pw.edu.pl>
2022-10-22 15:59:04 +0200

    Jednak nadal zle, to 2 rownanie w stan_ciagly nwm jak przerobic

--- Matlab/Part_1/stan_ciagly.m
+++ Matlab/Part_1/stan_ciagly.m
@@ -6,7 +6,6 @@ function dh = stan_ciagly(t,h,F1_ust,FD,A1,C2,ap1,ap2,tau)
         F1 = 0;
     end
     dh = zeros(2, 1);
-    dh(1)= (F1 + FD - ap1*(sqrt(h(1))))/A1;
-    %dh(2)= (ap1*(sqrt(h(1))) - ap2*nthroot(h(2),4))/C2;
-    dh(2)= (ap1*sqrt(h(1)) - ap2*sqrt(h(2)))/C2;
+    dh(1)= (F1 + FD - ap1 * sqrt(h(1))) / A1;
+    dh(2)= sqrt(ap1 * sqrt(h(1) - ap2 * sqrt (h(2)) ) /C2);
 end
--- Matlab/Part_1/symulacja_obiektu.m
+++ Matlab/Part_1/symulacja_obiektu.m
@@ -3,19 +3,20 @@ clear all;
 
 %Constants
 A1 = 505;
-C2 = 606;
+C2 = 0.65;
 ap1 = 23; %alfa_1
 ap2 = 15; %alfa_2
 
 %Punkt pracy
 tau = 120;
 h2_0 = 38.44;
+h1_0 = 0;
 F1 = 78;
 FD = 15;
 
 %Obliczanie Dynamiki Objętości
 funkcja = @(t,h)stan_ciagly(t,h,F1,FD,A1,C2,ap1,ap2,tau) ;
-[t,h] = ode45(funkcja,[0, 5000],[0;h2_0]);
+[t,h] = ode45(funkcja,[0, 10000],[h1_0;h2_0]);
 
 %Plot h/t
 
@@ -23,4 +24,5 @@ figure;
 plot(t,h(:,1));
 hold on;
 plot(t,h(:,2));
-legend;
+legend("h_1","h_2");
+xlabel("Czas"); ylabel("Wysokość");
