ff7c37b4826ce4234102f321dd37b15e54807630
Adam <01158187@pw.edu.pl>
2022-10-22 16:26:45 +0200

    Poprawione
    Chyba jest okrej, statyka wyglada git (stabilizuje sie tam gdzie powinno)

--- Matlab/Part_1/symulacja_obiektu.m
+++ Matlab/Part_1/symulacja_obiektu.m
@@ -11,15 +11,22 @@ ap2 = 15; %alfa_2
 tau = 120;
 h2_0 = 38.44;
 h1_0 = 0;
+
+v2_0 = h2_0^2 * C2;
+v1_0 = h1_0^2 * A1;
+
 F1 = 78;
 FD = 15;
 
 %Obliczanie Dynamiki Objętości
-funkcja = @(t,h)stan_ciagly(t,h,F1,FD,A1,C2,ap1,ap2,tau) ;
-[t,h] = ode45(funkcja,[0, 10000],[h1_0;h2_0]);
+funkcja = @(t,v)stan_ciagly(t,v,F1,FD,A1,C2,ap1,ap2,tau) ;
+[t,v] = ode45(funkcja,[0, 10000],[v1_0;v2_0]);
+
+%Obliczanie Wysokości na podstawie objętości
+h(:,1) = v(:,1) / A1;
+h(:,2) = sqrt(v(:,2) /C2);
 
 %Plot h/t
-
 figure;
 plot(t,h(:,1));
 hold on;
