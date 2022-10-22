43e38aaa070dcc4ca45649d9e0fa195779db53df
Adam <01158187@pw.edu.pl>
2022-10-22 13:10:36 +0200

    Liczenie obiektu (cos na poczatku nie tak)

--- Matlab/Part_1/symulacja_obiektu.m
+++ Matlab/Part_1/symulacja_obiektu.m
@@ -11,4 +11,14 @@ ap2 = 15; %alfa_2
 F1 = 78;
 FD = 15;
 
-ode45(@dy,[0,10],[0,0])
+%Obliczanie Dynamiki Objętości
+funkcja = @(t,y) [F1 + FD - ap1 * sqrt(y(1)/A1); ap1 * sqrt(y(1)/A1)- ap2*(sqrt(sqrt(y(2)/C2)))];
+[t,v] = ode45(funkcja,[0, 5000],[0 0]);
+
+%Obliczanie wysokości
+h(:,1) = v(:,1)/A1;
+h(:,2) = sqrt(v(:,2)/C2);
+
+%Plot h/t
+
+plot(t,h);
