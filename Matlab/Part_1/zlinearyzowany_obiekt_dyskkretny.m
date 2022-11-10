a581cbc9c6124f6db35a7c72ca34ebf96b1f447e
Adam <01158187@pw.edu.pl>
2022-11-10 09:05:38 +0100

    Update zlinearyzowany_obiekt_dyskkretny.m

--- Matlab/Part_1/zlinearyzowany_obiekt_dyskkretny.m
+++ Matlab/Part_1/zlinearyzowany_obiekt_dyskkretny.m
@@ -18,16 +18,16 @@ F10 = 78;
 FD0 = 15;
 
 %Objetośc punktu pracy
-v2_0 = h2_0^2 * C2;
-v1_0 = h1_0 * A1;
+% v2_0 = h2_0^2 * C2;
+% v1_0 = h1_0 * A1;
 
 t_sym = 5000; %czas symulacji
 T = 1; %krok
 
 %warunki_początkowe
 kp = tau/T + 2;
-v1(1:kp) = v1_0;
-v2(1:kp) = v2_0;
+% v1(1:kp) = v1_0;
+% v2(1:kp) = v2_0;
 h1(1:kp) = h1_0;
 h2(1:kp) = h2_0;
 F1in(1:T:t_sym/T) = F1;
@@ -36,18 +36,24 @@ figure;
 
 for P = 36:21:120
     h2(1:kp) = h2_0;
+    h1(1:kp) = h1_0;
     for k = kp:t_sym/T
         if k/T > 180
             F1in(k) = P;
         end
-        v1(k) = v1(k-1) + T*(F1in(k-1-(tau/T)) - F10 + FDc(k-1) - FD0 - (ap1/(2*(sqrt(h1_0))))*(h1(k-1)-h1_0));
-        v2(k) = v2(k-1) + T*((ap1/(2*(sqrt(h1_0))))*(h1(k-1)-h1_0) - (ap2/(2*(sqrt(h2_0))))*(h2(k-1)-h2_0)); 
-        h2(k) = h2_0 + 1/(2*sqrt(C2*v2_0))*(v2(k) - v2_0);
-        h1(k) = v1(k)/A1;
+%         v1(k) = v1(k-1) + T*(F1in(k-1-(tau/T)) - F10 + FDc(k-1) - FD0 - (ap1/(2*(sqrt(h1_0))))*(h1(k-1)-h1_0));
+%         v2(k) = v2(k-1) + T*((ap1/(2*(sqrt(h1_0))))*(h1(k-1)-h1_0) - (ap2/(2*(sqrt(h2_0))))*(h2(k-1)-h2_0)); 
+%         h2(k) = h2_0 + 1/(2*sqrt(C2*v2_0))*(v2(k) - v2_0);
+%         h1(k) = v1(k)/A1;
+
+        % Version 2.0
+          h1(k) = h1(k-1) + T*(F1in(k-1-(tau/T)) + FDc(k-1) - ap1 * (sqrt(h1_0) + (h1(k-1) - h1_0)/(2*sqrt(h1_0))))/A1;
+          h2(k) = h2(k-1) + T*((ap1 * sqrt(h1_0) - ap2 * sqrt(h2_0))/(2*C2*h2_0)   +    ap1*(h1(k-1) - h1_0)/(4*C2*(sqrt(h1_0)))    -   ap2*(h2(k-1) - h2_0)/(4*C2*(nthroot((h2_0 * h2_0),3))));
     end
     plot((1:k),h2);
     hold on
     clear h2
+    clear h1
 end
 
 xlabel("k"); ylabel("h_2");
